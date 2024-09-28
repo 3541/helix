38e6be082a6405a3d004fc563b421afae560ef98
Alex O'Brien <3541@3541.website>
2024-09-28 19:17:50 +1000

    Fixup

--- helix-term/src/ui/editor.rs
+++ helix-term/src/ui/editor.rs
@@ -395,13 +395,6 @@ impl EditorView {
 
         let mut spans = syntax.rainbow_spans(text, Some(syntax_node_range), theme.rainbow_length());
 
-        for (_highlight, range) in spans.iter_mut() {
-            let start = text.byte_to_char(ensure_grapheme_boundary_next_byte(text, range.start));
-            let end = text.byte_to_char(ensure_grapheme_boundary_next_byte(text, range.end));
-
-            *range = start..end;
-        }
-
         spans
     }
 
